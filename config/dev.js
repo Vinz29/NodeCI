eea3dfcc2c5bc4d2b46b56ebc6cc93bd6a2fe52c
Even Vincent <vincent.even@al-enterprise.com>
2018-11-27 09:33:25 +0100

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://vincent:Brest29@ds113454.mlab.com:13454/vincent_blog_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
